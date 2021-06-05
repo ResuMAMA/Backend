15648fa977ba7eb916ae9032c79738c07e89891f
Rishika Wadhera <rishika.wadhera6@gmail.com>
2021-06-05 19:49:12 +0530

    db connected

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db="mongodb+srv://Rishika:resuMAMA@cluster0.2pxmk.mongodb.net/resuMAMA?retryWrites=true&w=majority"
+mongoose.connect(db,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useCreateIndex:true,
+  useFindAndModify:false
+}).then(()=>{console.log("db connected")}).catch((err)=>{
+  console.log(err);}
+  )
 
 // auth routes
 const authRoute=require('./routes/auth');
-app.use('/api',authRoute);
+app.use('/auth',authRoute);
 
 const PORT =process.env.PORT||8080;
 app.get('/', (req, res) => {
